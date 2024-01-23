58574278999c92cf4083067cd2801a87429207e3
YuHsuan87 <64847295+YuHsuan87@users.noreply.github.com>
2024-01-23 14:43:17 +0800

    Add piece update and compute funtion

--- cogs/member.py
+++ cogs/member.py
@@ -15,65 +15,65 @@ def send_picture(pic_name):
 
 # load the user's information from database.csv
 def show_info(user_name):
-        with open('cogs\database.csv', newline='', encoding='utf-8') as csvfile:
-            # 讀取 CSV 檔案內容
-            rows = csv.reader(csvfile)
-            list_data = list(rows)
-
-        have_user = False
-        for i in range(1, len(list_data)):
-            if(list_data[i][0] == user_name):
-                have_user = True
-                user_info = list_data[i][1:]
-                break
-
-        return user_info if have_user else []
+    with open('cogs\database.csv', newline='', encoding='utf-8') as csvfile:
+        # 讀取 CSV 檔案內容
+        rows = csv.reader(csvfile)
+        list_data = list(rows)
+    have_user = False
+    for i in range(1, len(list_data)):
+        if(list_data[i][0] == user_name):
+            have_user = True
+            user_info = list_data[i][1:]
+            break
+    return user_info if have_user else []
 
 # attempt to update the information about the users. but updating the info by themselves. 
 def update_info(user_name, attr, attr_info):
-        if(attr == 'level'):
-            if(check_level_format(attr_info)):
-                return f'Your level format is wrong!!\n Correct format is **level_exp** (exp 到小數點後第三位).'
-            
-        info = [attr, attr_info]
-        r = csv.reader(open('cogs\database.csv', encoding='utf-8'))
-        lines = list(r)
-        field = lines[0]
-        if info[0] not in field:
-            return f'Error...Have not {info[0]} attribute.'
-
-        for line in lines:
-            if(line[0] == user_name):
-                line[field.index(info[0])] = info[1]
-
-        with open('cogs\database.csv', 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerows(lines)
-
-        return f'Updated {user_name}\'s {info[0]} done.'
+    if(attr == 'level'):
+        if(check_level_format(attr_info)):
+            return f'Your level format is wrong!!\n Correct format is **level_exp** (exp 到小數點後第三位).'
+    elif(attr == 'hex_3'):
+        if(check_hex_3_level(attr_info)):
+            return f'Your hex_3 level format is wrong, please check **num1-num2-num3-num4**.'
+    info = [attr, attr_info]
+    r = csv.reader(open('cogs\database.csv', encoding='utf-8'))
+    lines = list(r)
+    field = lines[0]
+    if info[0] not in field:
+        return f'Error...Have not {info[0]} attribute.'
+    for line in lines:
+        if(line[0] == user_name):
+            line[field.index(info[0])] = info[1]
+    with open('cogs\database.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(lines)
+    return f'Updated {user_name}\'s {info[0]} done.'
 
 # level's format: level_exp, level can't empty, and then exp is round up to the third decimal (ex. 265_28.733).
 def check_level_format(level_str: str):
-        if('_' not in level_str):
-            return True
-        tmp = level_str.split('_')
-        if(len(tmp[1]) != 6 or len(tmp[0]) == 0):
-            return True
-        
-        return False
+    if('_' not in level_str):
+        return True
+    tmp = level_str.split('_')
+    if(len(tmp[1]) != 6 or len(tmp[0]) == 0):
+        return True
+    
+    return False
+
+def check_hex_3_level(level_str: str):
+    if(level_str.count('-') != 3):
+        return True
+    return False
 
 # compute the corresponding elda pieces to the hexa skill level.
 def compute_elda(hex_1_level: int , hex_2_level: int, hex_3_level: list):
-        hex_1 = [0, 30, 35, 40, 45, 50, 55, 60, 65, 200, 80, 90, 100, 110, 120, 130, 140, 150, 160, 350, 170, 180, 190, 200, 210, 220, 230, 240, 250, 500]
-        hex_2 = [50, 15, 18, 20, 23, 25, 28, 30, 33, 100, 40, 45, 50, 55, 60, 65, 70, 75, 80, 175, 85, 90, 95, 100, 105, 110, 115, 120, 125, 250]
-        hex_3 = [75, 23, 27, 30, 34, 38, 42, 45, 49, 150, 60, 68, 75, 83, 90, 98, 105, 113, 120, 263, 128, 135, 143, 150, 158, 165, 173, 180, 188, 375]
-
-        hex_3_level = [int(num) for num in hex_3_level]
-        hex_3_sum = 0
-        for level in hex_3_level:
-            hex_3_sum += sum(hex_3[:level])
-
-        return sum(hex_1[:hex_1_level]) + sum(hex_2[:hex_2_level]) + hex_3_sum
+    hex_1 = [0, 30, 35, 40, 45, 50, 55, 60, 65, 200, 80, 90, 100, 110, 120, 130, 140, 150, 160, 350, 170, 180, 190, 200, 210, 220, 230, 240, 250, 500]
+    hex_2 = [50, 15, 18, 20, 23, 25, 28, 30, 33, 100, 40, 45, 50, 55, 60, 65, 70, 75, 80, 175, 85, 90, 95, 100, 105, 110, 115, 120, 125, 250]
+    hex_3 = [75, 23, 27, 30, 34, 38, 42, 45, 49, 150, 60, 68, 75, 83, 90, 98, 105, 113, 120, 263, 128, 135, 143, 150, 158, 165, 173, 180, 188, 375]
+    hex_3_level = [int(num) for num in hex_3_level]
+    hex_3_sum = 0
+    for level in hex_3_level:
+        hex_3_sum += sum(hex_3[:level])
+    return sum(hex_1[:hex_1_level]) + sum(hex_2[:hex_2_level]) + hex_3_sum
 
 # 定義名為 Info 的 Cog
 class Member(commands.Cog):
@@ -141,6 +141,8 @@ class Member(commands.Cog):
             if(line[0] == name_str):
                 before = line[4]
                 game_name = line[1]
+                before_piece = int(line[11])
+
         before_level_exp = before.split('_')
         before_level, before_exp = before_level_exp[0], before_level_exp[1]
 
@@ -157,19 +159,21 @@ class Member(commands.Cog):
         else:
             update_exp = f"{cur_level}: {round(float(cur_exp) - float(before_exp), 3)}%"
 
+        update_piece = piece - before_piece
         # Write the data into the record csv.
-        record_data = [name_str, str_datetime, space, f'{time}', f'{piece}', update_exp, money, item_drop, money_drop]
+        record_data = [name_str, str_datetime, space, time, update_piece, update_exp, money, item_drop, money_drop]
         record_level_file = os.path.join('cogs', 'record.csv')
         with open(record_level_file, 'a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(record_data)
 
-        show_text = f'{game_name} {str_datetime}\n- 地圖: {space}\n- 練等時間: {time} HR\n- 愛爾達碎片獲取量: {piece}\n- 經驗提升量: {update_exp}\n- 獲取金錢數: {money}\n- 道具掉落率: {item_drop} %\n- 楓幣掉落率: {money_drop} %.'
+        show_text = f'{game_name} {str_datetime}\n- 地圖: {space}\n- 練等時間: {time} HR\n- 愛爾達碎片獲取量: {update_piece}\n- 經驗提升量: {update_exp}\n- 獲取金錢數: {money}\n- 道具掉落率: {item_drop} %\n- 楓幣掉落率: {money_drop} %.'
         await ctx.send(show_text)
 
         # Final, update the level information in database.csv
         updated_status = update_info(name_str, 'level', cur)
-        await ctx.send(updated_status)
+        updated_status = update_info(name_str, 'piece', update_piece)
+        await ctx.send(f'Record of training status is done! Thank you~ :D')
 
 
 # Cog 載入 Bot 中
